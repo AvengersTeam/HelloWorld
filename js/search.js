fedae2b798a0a0c0eabb70fe00e71c16edbe2f6d
Fernando <fgmoralesc@gmail.com>
2015-04-17 16:09:54 -0300

    Only urls can be clicked

--- js/search.js
+++ js/search.js
@@ -1,11 +1,12 @@
-$('#sparql').val('SELECT ?Nombre ?D WHERE { ?NombreUri foaf:firstName ?Nombre . ?NombreUri bio:event ?b . ?b bio:birth ?D FILTER (?D = "1952"^^xsd:string ) }')
+$('#sparql').val('SELECT ?Nombre ?FechaNacimiento\nWHERE {\n    ?NombreUri foaf:firstName ?Nombre .\n    ?NombreUri bio:event ?b .\n    ?b bio:birth ?FechaNacimiento\n    FILTER (?FechaNacimiento = "1952"^^xsd:string )\n}')
+//SELECT ?a ?b ?c WHERE { ?a ?b ?c FILTER (str(?a) = "http://www.semanticweb.org/sisib/ontologies/2015/3/ubibtest#Ahmad_'Abd_al-Majid" ) }
 
 function clearAllTables() {
-  var allTablesID = ['#subject-table', '#predicate-table', '#object-table'];
+  var allTablesID = ['#subject-table', '#predicate-table', '#object-table', '#sparql-table'];
   var allInputsID = ['#subject', '#predicate', '#object'];
   for ( key in allTablesID ) {
     $( allTablesID[key] ).empty();
-    $( allInputsID[key] ).val('')
+    $( allInputsID[key] ).val('');
   }
 }
 
@@ -16,13 +17,13 @@ function clearTable(ID) {
 function doSubjectQuery() { 
   var value = $('#subject').val();
   var q = 'SELECT DISTINCT ?Subject ?Predicate ?Object WHERE { ?Subject ?Predicate ?Object . FILTER regex(str(?Subject), "'+ value +'", "i" ) } LIMIT 10';
-  doQuery(q,'#subject')
+  doQuery(q,'#subject');
 }
 
 function doPredicateQuery() { 
   var value = $('#predicate').val();
   var q = 'SELECT DISTINCT ?Subject ?Predicate ?Object WHERE { ?Subject ?Predicate ?Object . FILTER regex(str(?Predicate), "'+ value +'", "i" ) } LIMIT 10';
-  doQuery(q,'#predicate')
+  doQuery(q,'#predicate');
 }
 
 function doObjectQuery() { 
@@ -34,7 +35,7 @@ function doObjectQuery() {
 function doSparql() { 
   var value = $('#sparql').val();
   var q = value;
-  doQuery(q,'#sparql')
+  doQuery(q,'#sparql');
 }
 
 function doQuery(q,resContainerId) {
@@ -78,7 +79,8 @@ function doQuery(q,resContainerId) {
       $( data.results.bindings ).each( function( k, v ) {
         table += '<tr>';
         $( props ).each( function ( i, it ) { 
-          table += '<td><a href="' + v[it].value + '" target="_blank">' + v[it].value + '</a></td>'; 
+          if (v[it].value.substr(0,7)=='http://') table += '<td><a href="' + v[it].value + '" target="_blank">' + v[it].value + '</a></td>';
+          else table += '<td>' + v[it].value + '</td>';
         } );
         table += '</tr>';  
       });
